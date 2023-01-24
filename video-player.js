1510359e739dcd14391696aa514734fb860d211a
pouya sorbi <99685495+opiya0@users.noreply.github.com>
2023-01-24 22:46:08 +0330

    Update video-player.js

--- video-player.js
+++ video-player.js
@@ -1,5 +1,20 @@
 let video=document.getElementById("video")
 let control=document.getElementById("control")
-video.addEventListener("mousemove",function(){
-control.style.opacity="50%";
+let video2=document.getElementById("video2")
+control.addEventListener("mousemove",function(){
+control.style.opacity="65%";
 })
+control.addEventListener("mouseleave",function(){
+    control.style.opacity="0%";
+    })
+    function playvideo(){
+    if(video2.paused){
+        video2.play();
+    }
+    else{
+        video2.pause();
+    }
+    
+    }
+    
+    
