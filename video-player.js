47e4b3c218abba54cb6b5c1c2817f3b0b1d0c987
pouya sorbi <99685495+opiya0@users.noreply.github.com>
2023-02-02 18:20:42 +0330

    Update video-player.js

--- video-player.js
+++ video-player.js
@@ -1,6 +1,7 @@
 let video=document.getElementById("video")
 let control=document.getElementById("control")
 let video2=document.getElementById("video2")
+let text=document.getElementById("text")
 control.addEventListener("mousemove",function(){
 control.style.opacity="65%";
 })
@@ -29,14 +30,52 @@ document.onkeydown=function(e){
         case "ArrowUp":
      if(video2.volume <1){
         video2.volume=(video2.volume+0.2).toFixed(1);//ta 1 ragm ashar neshon mide
+        text.innerHTML=video2.volume*100+"%";
      }
+     break;
      case "ArrowDown":
         if(video2.volume >0){
             video2.volume=(video2.volume-0.2).toFixed(1);//ta 1 ragm ashar neshon mide
+            text.innerHTML=video2.volume*100+"%";
         }
+        break;
+
+        case"ArrowRight":
+        movevideo(1);
+
+        break;
+
+        case"ArrowLeft":
+        movevideo(-1);
+        break;
+        case"Enter":
+        fullscreen();
     }
 
 
 }
-    
+
+function fullscreen(){
+    if(video2.requestFullscreen){
+        video2.requestFullscreen();
+    }
+    if(document.exitFullscreen){
+        document.exitFullscreen();
+    }
+}  
+
+function stopvideo(){
+    video2.currentTime=0;
+  }  
+  function muted(){
+    if(video2.volume===0){
+        video2.volume=1;
+        text.innerHTML=video2.volume*100+"%";
+    }
+    else{
+        video2.volume=0;
+        text.innerHTML=video2.volume*100+"%";
+    }
+
+  }
     
